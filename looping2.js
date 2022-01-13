acf7c821384617d7ee212643612f621cb5899c9e
riifalll <m.rifaljunaidi83@gmail.com>
2022-01-13 17:23:39 +0700

    selesai tapi pusing

---
+++ looping2.js
@@ -0,0 +1,26 @@
+//looping 1
+// var rows1= 5;
+
+// for (var i=0 ;i<rows1 ;i++){
+//     console.log('*')
+// }
+
+//looping 2
+// var rows2= 5;
+// for (var i=0; i<rows2; i++){
+//     var bintang=""
+//     for (var  j=0; j<rows2; j++){
+//         bintang+="*"
+//     }
+//       console.log(bintang)
+// }
+
+//looping 3
+var rows3=5;
+for (var i=0; i<rows3; i++){
+    var bintang="*"
+    for (var j=0; j<i; j++){
+       bintang+='*'
+    }
+      console.log(bintang)
+}
