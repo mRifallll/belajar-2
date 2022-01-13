312c01c103822fa3546afa1119a9f79304d2c55c
riifalll <m.rifaljunaidi83@gmail.com>
2022-01-13 15:04:42 +0700

    selesai looping

---
+++ looping1.js
@@ -0,0 +1,29 @@
+
+// for (var i = 0; i < 101; i++) {
+//   if (i % 2 == 0){
+//       console.log("bilangan genap "  + i)
+//   } else {
+//     console.log( "bilangan ganjil " + i)
+//   }
+// }
+
+
+for (var i = 1; i < 101; i+=2) {
+  if (i % 3 == 0){
+      console.log("3 kelipatan 3 "  + i)
+  } 
+}
+
+for (var i = 1; i < 101; i+=5) {
+  if (i % 6 == 0){
+      console.log("6 kelipatan 6 "  + i)
+  } 
+}
+
+for (var i = 1; i < 101; i+=9) {
+  if (i % 10 == 0){
+      console.log("10 kelipatan 10 "  + i)
+  } 
+  
+}
+
