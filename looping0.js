5a22244454097f2bb6bff96d325f32b168c93222
riifalll <m.rifaljunaidi83@gmail.com>
2022-01-14 13:36:33 +0700

    selesai

---
+++ looping0.js
@@ -0,0 +1,27 @@
+//1. Melakukan Looping Menggunakan While
+
+// console.log('looping petama')
+// var loop = 2 ;
+// while(loop < 10){
+//   console.log(loop + '  I love coding');
+// loop++
+// }
+// console.log('looping kedua')
+// while(loop >= 2){
+//   console.log(loop + ' I will become fullstack developer');
+// loop--
+// }
+
+//2. Melakukan Looping Menggunakan For
+
+// console.log('LOOPING PERTAMA')
+// for(var i = 0;  i <20; i++)
+// {
+//     console.log(i + ' I love coding');
+// }
+// console.log('LOOPING KEDUA')
+
+// for(var j =20; j >0; j--){
+//     console.log(j + ' I will become fullstack developer');
+
+// }
